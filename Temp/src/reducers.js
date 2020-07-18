d2671bdf9b1a156881ae17bc934abb9019f6c2c2
parshuramreddysudda <parshuram.sudda@gmail.com>
2020-07-19 01:15:16 +0530

    Removed temp files

--- Temp/src/reducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import applicationReducer from './Application/Services/application.reducer'
-// import component2Reducer from "Component2"
-
-export default combineReducers({
-   application:applicationReducer,
-   // componentname:component2Reducer
-});
